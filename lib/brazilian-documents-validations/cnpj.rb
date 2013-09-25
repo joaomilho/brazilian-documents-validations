728fe31dab36abd8925dd176830f7eb048378424
Maiz Lulkin <maiz@lulk.in>
2013-09-26 01:16:03 +0200

    Refactor cnpj to use abstract class

--- lib/brazilian-documents-validations/cnpj.rb
+++ lib/brazilian-documents-validations/cnpj.rb
@@ -8,77 +8,22 @@ module CNPJ
     not valid?(cnpj)
   end
 
-  class Validator
-
-    VALID_FORMATS = /^\d{13,14}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
+  class Validator < DocumentValidator
 
     def initialize cnpj
-      @cnpj = cnpj.to_s
-    end
-
-    def valid?
-      return false if @cnpj !~ VALID_FORMATS
-      return false if same_number?
-      return false if first_digit_calculated != first_digit
-
-      second_digit_calculated == second_digit
+      @document = cnpj.to_s
+      @size = 14
     end
 
     private
 
-    def first_digit
-      integer_cnpj[12]
+    def valid_format?
+      @document =~ /^\d{13,14}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
     end
 
-    def second_digit
-      integer_cnpj[13]
+    def factor
+      [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
     end
 
-    def integer_cnpj
-      return @integer_cnpj if defined?(@integer_cnpj)
-
-      @integer_cnpj = @cnpj.scan(/\d/).collect(&:to_i)
-      @integer_cnpj.unshift(0) if @integer_cnpj.length == 13
-      @integer_cnpj
-    end
-
-    def same_number?
-      integer_cnpj.uniq.size == 1
-    end
-
-    def first_sum
-      sum 11, first_factor
-    end
-
-    def second_sum
-      sum 12, second_factor
-    end
-
-    def sum chars, factor
-      (0..chars).inject(0) do |sum, i|
-        sum + integer_cnpj[i] * factor[i]
-      end
-    end
-
-    def first_factor
-      @first_factor ||= [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-    end
-
-    def second_factor
-      @second_factor ||= first_factor.unshift 6
-    end
-
-    def first_digit_calculated
-      calculate_mod(first_sum)
-    end
-
-    def second_digit_calculated
-      calculate_mod(second_sum)
-    end
-
-    def calculate_mod value
-      mod = value % 11
-      mod < 2 ? 0 : 11 - mod
-    end
   end
 end
